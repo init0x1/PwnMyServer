b6b513eb39c31f7b16175b09ba4ef5825761c610
0xAbdoAli <abdelrahman.elshaikh7@gmail.com>
2023-11-02 18:02:26 +0200

    Add loop for continuous command input

--- main.go
+++ main.go
@@ -7,24 +7,29 @@ import (
 )
 
 func main() {
-	fmt.Print("Enter a command: ")
-	var command string
-	_, err := fmt.Scanln(&command)
-	if err != nil {
-		fmt.Printf("Error reading input: %v\n", err)
-		return
-	}
+	for {
+		fmt.Print("Enter a command to be executed or exit to terminate : ")
+		var command string
+		_, err := fmt.Scanln(&command)
+		if err != nil {
+			fmt.Printf("Error reading input: %v\n", err)
+			return
+		}
+		if command == "exit" {
+			break
+		}
 
-	apiRes, commandError, err := api.Eval(command)
-	if err != nil {
-		fmt.Printf("API request failed: %v\n", err)
-		return
-	}
-	if commandError != nil {
+		apiRes, commandError, err := api.Eval(command)
+		if err != nil {
+			fmt.Printf("API request failed: %v\n", err)
+			return
+		}
+		if commandError != nil {
 
-		fmt.Println(commandError.Error)
+			fmt.Println(commandError.Error)
 
-	} else {
-		fmt.Println("Command result:", apiRes.CommandResult)
+		} else {
+			fmt.Println("Command result:", apiRes.CommandResult)
+		}
 	}
 }
